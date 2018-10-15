5d1d12c2f89a1546e6ce2b8c4981c03608b81f4c
Mattias <mattias@zvea.co>
2018-10-15 18:55:30 +0200

    ...

--- prebuilt/pre.mk
+++ prebuilt/pre.mk
@@ -91,12 +91,12 @@ PRODUCT_COPY_FILES += \
 
 #Fonts
 PRODUCT_COPY_FILES += \
-    vendor/descendant/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
-    vendor/descendant/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
-    vendor/descendant/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/descendant/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
-    vendor/descendant/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    vendor/descendant/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+    vendor/descendant/fonts/GoogleSans/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/descendant/fonts/GoogleSans/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/descendant/fonts/GoogleSans/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/descendant/fonts/GoogleSans/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/descendant/fonts/GoogleSans/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/descendant/fonts/GoogleSans/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
 
     #Sounds
 
